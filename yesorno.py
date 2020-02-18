1ab5264b71f57a93fb559bc2f1364259158e598b
Chohyeonjeong18 <43758445+Chohyeonjeong18@users.noreply.github.com>
2020-02-18 11:15:19 +0900

    Update and rename YesOrNo.py to yesorno.py

--- yesorno.py
+++ yesorno.py
@@ -2,7 +2,7 @@ import requests
 
 # This function will pass your text to the machine learning model
 # and return the top result with the highest confidence
-def YesOrNO_classify(text):
+def yesorno_classify(text):
     key = "3ddb25e0-51ee-11ea-a23e-3f15abe9a3ade5b3619c-8090-4842-bbab-a4777084cb4b"
     url = "https://machinelearningforkids.co.uk/api/scratch/"+ key + "/classify"
 
